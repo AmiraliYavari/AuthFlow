a9d0fc85148a1529db887aa68516c670b86acc32
iRush <amiraliyavariii91@gmail.com>
2026-07-27 20:42:06 +0330

    feat: setup application routing

---
+++ src/routes/router.tsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import AuthLayout from "../components/layout/AuthLayout";
+import AppLayout from "../components/layout/AppLayout";
+
+import LoginPage from "../features/auth/pages/LoginPage";
+import RegisterPage from "../features/auth/pages/RegisterPage";
+import ForgotPasswordPage from "../features/auth/pages/ForgotPasswordPage";
+import ProfilePage from "../features/auth/pages/ProfilePage";
+
+import NotFoundPage from "../pages/NotFoundPage";
+
+export const router = createBrowserRouter([
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+      {
+        path: "/forgot-password",
+        element: <ForgotPasswordPage />,
+      },
+    ],
+  },
+
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/profile",
+        element: <ProfilePage />,
+      },
+    ],
+  },
+
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
