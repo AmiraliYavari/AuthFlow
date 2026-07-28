6cdc69289eeacbc06b7bcb15657c925ca2039590
iRush <amiraliyavariii91@gmail.com>
2026-07-28 12:02:07 +0330

    feat: implement login form validation

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,29 +1,56 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import Button from "../../../components/ui/Button";
 import Card from "../../../components/ui/Card";
 import Input from "../../../components/ui/Input";
 import Logo from "../../../components/ui/Logo";
 
+import {
+  loginSchema,
+  type LoginSchema,
+} from "../schemas/loginSchema";
+
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  function onSubmit(data: LoginSchema) {
+    console.log(data);
+  }
+
   return (
     <Card>
-      <div className="space-y-5">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="space-y-5"
+      >
         <Logo />
 
         <Input
           label="Email"
-          placeholder="example@email.com"
+          placeholder="Enter your email"
+          {...register("email")}
+          error={errors.email?.message}
         />
 
         <Input
           label="Password"
           type="password"
-          placeholder="••••••••"
+          placeholder="Enter your password"
+          {...register("password")}
+          error={errors.password?.message}
         />
 
-        <Button fullWidth>
-          Login
+        <Button fullWidth type="submit">
+          Sign In
         </Button>
-      </div>
+      </form>
     </Card>
   );
 }
